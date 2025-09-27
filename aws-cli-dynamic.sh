80b35c6c0d207cafca64ab6b4eb166ea06a5f55c
unknown <vikas.arisela@gmail.com>
2025-09-27 09:12:43 +0400

    shell-awscli

---
+++ aws-cli-dynamic.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+for instance in @ 
+do 
+ Instance_ID=(aws ec2 run-instances \
+  --image-id ami-09c813fb71547fc4f \
+  --instance-type t2.micro \
+  --security-group-ids sg-07e88823124a6dddf \
+  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+  --query 'Instances[0].InstanceId' \
+  --output text
+)
+
+if [ $instance -ne "frontend" ];then
+IP=$(aws ec2 describe-instances \
+  --instance-ids i-0d732f11181977337 \
+  --query 'Reservations[0].Instances[0].[PrivateIpAddress]' \
+  --output text)
+
+else
+IP=$(aws ec2 describe-instances \
+  --instance-ids i-0d732f11181977337 \
+  --query 'Reservations[0].Instances[0].[PublicIpAddress]' \
+  --output text)
+fi
+echo "$instance : $IP" 
+done
