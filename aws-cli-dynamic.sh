ce9d32094974ef0e1ddb5431b1fe959a6068f509
unknown <vikas.arisela@gmail.com>
2025-09-27 09:22:38 +0400

    shell-awscli

--- aws-cli-dynamic.sh
+++ aws-cli-dynamic.sh
@@ -3,26 +3,26 @@
 
 for instance in @ 
 do 
- Instance_ID=(aws ec2 run-instances \
-  --image-id ami-09c813fb71547fc4f \
-  --instance-type t2.micro \
-  --security-group-ids sg-07e88823124a6dddf \
-  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
-  --query 'Instances[0].InstanceId' \
-  --output text
+    Instance_ID=(aws ec2 run-instances \
+      --image-id ami-09c813fb71547fc4f \
+      --instance-type t2.micro \
+      --security-group-ids sg-07e88823124a6dddf \
+      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+      --query 'Instances[0].InstanceId' \
+      --output text
 )
 
 if [ $instance != "frontend" ];then
-IP=$(aws ec2 describe-instances \
-  --instance-ids i-0d732f11181977337 \
-  --query 'Reservations[0].Instances[0].[PrivateIpAddress]' \
-  --output text)
+    IP=$(aws ec2 describe-instances \
+      --instance-ids i-0d732f11181977337 \
+      --query 'Reservations[0].Instances[0].[PrivateIpAddress]' \
+      --output text)
 
 else
-IP=$(aws ec2 describe-instances \
-  --instance-ids i-0d732f11181977337 \
-  --query 'Reservations[0].Instances[0].[PublicIpAddress]' \
-  --output text)
+    IP=$(aws ec2 describe-instances \
+      --instance-ids i-0d732f11181977337 \
+      --query 'Reservations[0].Instances[0].[PublicIpAddress]' \
+      --output text)
 fi
 echo "$instance : $IP" 
 done
