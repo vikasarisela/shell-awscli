a82b6f8157c420b5df22e84d18b6263f3eca67f3
unknown <vikas.arisela@gmail.com>
2025-09-27 09:33:18 +0400

    shell-awscli

--- aws-cli-dynamic.sh
+++ aws-cli-dynamic.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-07e88823124a6dddf"
 
 for instance in $@ 
 do 
     Instance_ID=$(aws ec2 run-instances \
-      --image-id ami-09c813fb71547fc4f \
+      --image-id $AMI_ID \
       --instance-type t2.micro \
-      --security-group-ids sg-07e88823124a6dddf \
+      --security-group-ids $SG_ID \
       --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
       --query 'Instances[0].InstanceId' \
       --output text
